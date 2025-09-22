d311b3ea6e823b7d593e6ad36a6f9e82f96e731e
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2025-09-22 00:51:22 -0300

    first-commit

---
+++ register.py
@@ -0,0 +1,15 @@
+class Aluno:
+    codigo = 0
+    nome = ""
+    nota1 = 0.0
+    nota2 = 0.0
+
+    def Visualizar():
+        print(f"Aluno: {codigo}\nNome: {nome}")
+
+def Menu():
+    print("*MENU DE OPÇÕES*")
+    print("1 - Cadastrar aluno\n2 - ")
+
+def Main():
+    pass
