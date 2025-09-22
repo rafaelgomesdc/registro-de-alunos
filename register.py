25dab956b99841c4b6b4c6173930dad5c63e16ff
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2025-09-22 01:13:09 -0300

    funcao-Cadastrar

--- register.py
+++ register.py
@@ -3,13 +3,43 @@ class Aluno:
     nome = ""
     nota1 = 0.0
     nota2 = 0.0
+    media = 0.0
 
-    def Visualizar():
-        print(f"Aluno: {codigo}\nNome: {nome}")
+    def CalcularMedia(self):
+        self.media = (self.nota1 + self.nota2) / 2
+
+    def Visualizar(self):
+        print(f"Aluno: {self.codigo}\nNome: {self.nome}\nNota 1: {self.nota1}\nNota 2: {self.nota2}\nMédia: {self.media}")
+
+def Cadastrar(lista):
+    a = Aluno()
+    a.codigo = len(lista) + 1
+    a.nome = input("Nome: ")
+    a.nota1 = float(input("Nota 1: "))
+    a.nota2 = float(input("Nota 2: "))
+
+    lista.Append(a)
+
+def Consultar(lista):
+    pass
+
+def VisualizarTodos(lista):
+    pass
 
 def Menu():
     print("*MENU DE OPÇÕES*")
-    print("1 - Cadastrar aluno\n2 - ")
+    print("1 - Cadastrar aluno\n2 - Consultar aluno\n3 - Visualizar todos os alunos\n4 - Encerrar execução")
+
+    return int(input("Digite a opção desejada: "))
 
 def Main():
-    pass
+    lista_alunos = []
+
+    while True:
+        match Menu():
+            case 1:
+                Cadastrar(lista_alunos)
+            case 2:
+                Consultar(lista_alunos)
+            case 3:
+                VisualizarTodos(lista_alunos)
