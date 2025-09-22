7d76694b3c2760120d1e98a344605cc404fc9a84
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2025-09-22 01:56:09 -0300

    codigo-csharp

---
+++ csharp/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp;
+
+public class Aluno
+{
+    int codigo;
+    string? nome;
+    double nota1;
+    double nota2;
+    public void CalcularMedia()
+    { }
+    public void Visualizar()
+    { }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using csharp;
+
+internal class Program
+{
+    private static void Main()
+    { }
+
+    private static int Menu()
+    {
+        Console.WriteLine("*MENU DE OPÇÕES*");
+        Console.WriteLine("\n1 - Cadastrar aluno\n2 - Consultar aluno\n3 - Visualizar todos os alunos\n4 - Encerrar execução\n");
+        Console.Write("Digite a opção desejada: ");
+
+        return
+            Convert.ToInt32(Console.ReadLine());
+    }
+}
